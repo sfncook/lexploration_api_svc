96140b12fe8350a1bab5354d363ea98995fd7d97
Shawn Cook <sfncook@gmail.com>
2024-01-13 13:30:47 -0800

    CompaniesController

---
+++ Controllers/CompaniesController.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using SalesBotApi.Models;
+using Microsoft.Azure.Cosmos;
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace SalesBotApi.Controllers
+{
+    [Route("api/[controller]")] 
+    [ApiController]
+    public class CompaniesController : Controller
+    {
+        private readonly Container companiesContainer;
+
+        public CompaniesController(CosmosDbService cosmosDbService)
+        {
+            companiesContainer = cosmosDbService.CompaniesContainer;
+        }
+
+        // GET: api/companies
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Company>>> GetCompanies(
+            [FromQuery] string company_id
+        )
+        {
+            if (company_id == null)
+            {
+                return BadRequest("Missing company_id parameter");
+            }
+
+            string sqlQueryText;
+            if (company_id == "all") sqlQueryText = $"SELECT * FROM c";
+            else sqlQueryText = $"SELECT * FROM c WHERE c.company_id = '{company_id}'";
+
+            QueryDefinition queryDefinition = new QueryDefinition(sqlQueryText);
+
+            List<Company> companies = new List<Company>();
+            using (FeedIterator<Company> feedIterator = companiesContainer.GetItemQueryIterator<Company>(queryDefinition))
+            {
+                while (feedIterator.HasMoreResults)
+                {
+                    FeedResponse<Company> response = await feedIterator.ReadNextAsync();
+                    companies.AddRange(response.ToList());
+                }
+            }
+
+            Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            return companies;
+        }
+
+    }
+}
