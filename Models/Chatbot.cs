e26dbf8d45c7fde44c0016ba6fd00768d6e38d07
Shawn Cook <sfncook@gmail.com>
2024-01-14 12:11:47 -0800

    chatbot contact fields

--- Models/Chatbot.cs
+++ Models/Chatbot.cs
@@ -7,5 +7,9 @@ namespace SalesBotApi.Models
         public string company_id { get; set; }
         public bool show_avatar { get; set; }
         public string llm_model { get; set; }
+        public string contact_prompt { get; set; }
+        public string contact_link { get; set; }
+        public string contact_method { get; set; }
+        public string greeting { get; set; }
     }
 }
