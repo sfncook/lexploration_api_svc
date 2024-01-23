b4ac58dec789add57acba50b318747eda0b5fcc6
Shawn Cook <sfncook@gmail.com>
2024-01-23 13:03:24 -0800

    redirect_to_calendar add to chatbot

--- Models/Chatbot.cs
+++ Models/Chatbot.cs
@@ -20,6 +20,8 @@ namespace SalesBotApi.Models
         public bool collect_user_info { get; set; }
         public bool show_call_to_action { get; set; }
         public string contact_link { get; set; }
+        public bool redirect_to_calendar { get; set; }
+        public string calendar_link { get; set; }
         public string greeting { get; set; }
         public bool initialized { get; set; }
         public RedirectPrompts[] redirect_prompts { get; set; }
